7692d663f492179adf74d18f89b5c68e1b2bd917
jea111 <laliendra42@gmail.com>
2025-08-19 18:26:25 -0500

    Verano Fest

---
+++ js/script.js
@@ -0,0 +1,12 @@
+AOS.init();
+
+Toastify({
+  text: " ¡No olvides el evento 17 de octubre\n¡Compra tus entradas!",
+  duration: 8000,
+  close: true,
+  gravity: "top",
+  position: "right",
+  backgroundColor: "linear-gradient(to right, #00b09b, #96c93d)",
+
+  stopOnFocus: true,
+}).showToast();
